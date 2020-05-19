a873ffb1845bf271eafacc636cac8c99bd26fde0
Andy-Pruebas <andy.vilca@tecsup.edu.pe>
2020-05-19 15:30:14 -0500

    Primer Commit By Andy

---
+++ Lab8VAG/Lab8VAG/Lab8VAG/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace Lab8VAG
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            ExplicitDemo.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new ExplicitDemo());
+            };
+            ExplicitCode.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new ExplicitCode());
+            };
+            ExplicitControlDemo.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new ExplicitControlDemo());
+            };
+            ExplicitControlCode.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new ExplictControlCode());
+            };
+        }
+    }
+}
